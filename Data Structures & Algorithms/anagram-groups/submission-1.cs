46b5079fca0d20e6779b1ed529e5ceb0f6be8b7c
Daeksell <oaoazb@gmail.com>
2026-04-13 20:06:37 +0300

    Add: anagram-groups - submission-1

---
+++ Data Structures & Algorithms/anagram-groups/submission-1.cs
@@ -0,0 +1,31 @@
+public class Solution {
+    public List<List<string>> GroupAnagrams(string[] strs) {
+        return strs
+        .ToLookup(x => GetAnagramKey(x))
+        .Select(x => x.ToList())
+        .ToList();
+    }
+
+
+    private string GetAnagramKey(string value)
+    {
+        Span<int> array = stackalloc int[26];
+        foreach(var ch in value)
+        {
+            array[ch - 'a'] += 1;
+        }
+
+        var sb = new StringBuilder();
+        for(var i = 0; i < array.Length; i++)
+        {
+            var frequency = array[i];
+            if(frequency == 0)
+            {
+                continue;
+            }
+            var ch = i + 'a';
+            sb.Append($"{frequency}{ch}");
+        }
+        return sb.ToString();
+    }
+}
